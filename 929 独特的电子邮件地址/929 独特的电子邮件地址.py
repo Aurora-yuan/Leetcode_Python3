68a571205314e87a2294ae78d47cdd598561b6b9
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-10-10 20:05:33 +0800

    Create 929 独特的电子邮件地址.py

---
+++ 929 独特的电子邮件地址/929 独特的电子邮件地址.py
@@ -0,0 +1,16 @@
+#label: string difficulty: easy
+
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        res = set()
+        for email in emails:
+            local, domain = email.split("@")
+            local = local[:local.find('+')].replace(".","")
+            res.add(local + "@" + domain)
+        # print res
+        return len(res)
+
