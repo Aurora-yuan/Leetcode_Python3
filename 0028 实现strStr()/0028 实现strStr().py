d4c7d1904eebee15c04cf0916e5c915c8829f3e9
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-10-15 10:38:19 +0800

    Update 0028 实现strStr().py

--- 0028 实现strStr()/0028 实现strStr().py
+++ 0028 实现strStr()/0028 实现strStr().py
@@ -14,4 +14,29 @@ class Solution:
             return haystack.index(needle)
         return -1
     
+“”“
+第二种思路：
+
+把haystack可能为needle的子串全部找出来匹配一遍。
+
+”“”
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if not needle:
+            return 0
+        for i, char in enumerate(haystack):
+            if char == needle[0]:
+                if haystack[i:i + len(needle)] == needle:
+                    return i
+                
+        return -1
+
+
+  
     
