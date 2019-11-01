aef5512927a5632812af98248affa4ebad26f473
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-11-01 23:21:36 +0800

    Create 0509 斐波那契数.py

---
+++ 0509 斐波那契数/0509 斐波那契数.py
@@ -0,0 +1,10 @@
+#label: array difficulty: easy
+
+class Solution:
+    def fib(self, N: int) -> int:
+        if N == 0:
+            return 0
+        elif N == 1:
+            return 1
+        else:
+            return self.fib(N-1)+self.fib(N-2)
