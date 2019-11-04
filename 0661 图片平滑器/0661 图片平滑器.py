81b5d2739ef84dd149b780e47c18d20dd32f44a9
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-11-04 23:38:49 +0800

    Create 0661 图片平滑器.py

---
+++ 0661 图片平滑器/0661 图片平滑器.py
@@ -0,0 +1,38 @@
+#label: array difficulty: easy
+
+class Solution:
+    def imageSmoother(self, M: List[List[int]]) -> List[List[int]]:
+        result=[]
+        m,n=len(M),len(M[0])
+        for i in range(m):
+            result.append([])
+            for j in range(n):
+                value,count=M[i][j],1
+                if i-1>=0:
+                    value += M[i-1][j]
+                    count += 1
+                    if j-1>=0:
+                        value += M[i-1][j-1]
+                        count += 1
+                    if j+1<n:
+                        value += M[i-1][j+1]
+                        count += 1
+                if i+1<m:
+                    value += M[i+1][j]
+                    count += 1
+                    if j-1>=0:
+                        value += M[i+1][j-1]
+                        count += 1
+                    if j+1<n:
+                        value += M[i+1][j+1]
+                        count += 1
+                if j-1>=0:
+                    value += M[i][j-1]
+                    count += 1
+                if j+1<n:
+                    value += M[i][j+1]
+                    count += 1
+                result[i].append(value//count)
+        return result
+
+
