4ab54f2d1c53fdf09e554b48c507934ff38379fd
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-11-05 20:43:29 +0800

    Create 0561 数组拆分(1).py

---
+++ 0561 数组拆分(1)/0561 数组拆分(1).py
@@ -0,0 +1,10 @@
+#label: array difficulty: easy
+
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        res = 0
+        for i in range(0,len(nums),2):
+            res += nums[i]
+
+        return res
