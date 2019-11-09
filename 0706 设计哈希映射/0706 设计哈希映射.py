92c705bfe1771d26d9563ad53a44c87684772d77
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-11-09 23:06:16 +0800

    Create 0706 设计哈希映射.py

---
+++ 0706 设计哈希映射/0706 设计哈希映射.py
@@ -0,0 +1,36 @@
+#label: design difficulty: easy
+
+class MyHashMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.hashmap = [-99999 for _ in range(1000005)]  
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        self.hashmap[key] = value
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        if self.hashmap[key] != -99999:
+            return self.hashmap[key]
+        return -1
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+        self.hashmap[key] = -99999
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
