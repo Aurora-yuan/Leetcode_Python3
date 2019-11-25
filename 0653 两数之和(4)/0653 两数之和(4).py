cb8a7483681fdc9cdd68d5ffde8cb6ced5b0d655
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-11-25 11:49:39 +0800

    Create 0653 两数之和(4).py

---
+++ 0653 两数之和(4)/0653 两数之和(4).py
@@ -0,0 +1,31 @@
+#label: tree difficulty: easy
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution(object):
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        s = set()
+        queue = [root]
+        while queue:
+            node = queue.pop(0)
+            s.add(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        for num in s:
+            if k - num in s and 2 * (k - num) != k:
+                return True
+        return False
+
