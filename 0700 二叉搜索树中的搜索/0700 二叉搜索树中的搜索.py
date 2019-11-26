b0f220e803810b94d721decf20c7e41f111c9056
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-11-26 12:12:57 +0800

    Create 0700 二叉搜索树中的搜索.py

---
+++ 0700 二叉搜索树中的搜索/0700 二叉搜索树中的搜索.py
@@ -0,0 +1,23 @@
+#label: tree difficulty: easy
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        # print root.val
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
+            
+
+
