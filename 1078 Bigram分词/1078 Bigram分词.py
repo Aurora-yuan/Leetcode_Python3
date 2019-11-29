f591b58dc792e6b83cc8ea269373d5aa2a4907c1
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-11-29 10:57:23 +0800

    Create 1078 Bigram分词.py

---
+++ 1078 Bigram分词/1078 Bigram分词.py
@@ -0,0 +1,11 @@
+#label: string difficulty: easy
+
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        res = []
+        text = text.split(" ")
+        for i in range(len(text) - 2):
+            if text[i] == first and text[i+1] == second:
+                res.append(text[i+2])
+        
+        return res
