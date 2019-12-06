750f19a111afb1d03d7f12b0b6631ed6add9bc13
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-12-06 10:29:36 +0800

    Create 0103 二叉树的锯齿形层次遍历.py

---
+++ 0103 二叉树的锯齿形层次遍历/0103 二叉树的锯齿形层次遍历.py
@@ -0,0 +1,37 @@
+#label: stack difficulty: medium
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        queue = [root]
+        lor = 0 # 0 for left and 1 for right
+        res = list()
+        if not root:
+            return []
+        while(queue):
+            next_queue = list()
+            layer = list()
+            
+            for node in queue:
+                if node.left:
+                    next_queue.append(node.left)
+                if node.right:
+                    next_queue.append(node.right)  
+                layer.append(node.val)
+    
+            if not lor: # left
+                res.append(layer)
+            else:
+                res.append(layer[::-1])
+            
+            lor = 1 - lor
+            queue = next_queue
+        return res
+
+
