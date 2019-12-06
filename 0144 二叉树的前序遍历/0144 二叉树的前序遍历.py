e520c32ef9a33baed8cab1ad98745f832c2d4ce6
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-12-06 10:09:04 +0800

    Create 0144 二叉树的前序遍历.py

---
+++ 0144 二叉树的前序遍历/0144 二叉树的前序遍历.py
@@ -0,0 +1,18 @@
+#label: recurrence difficulty: medium
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        res = list()
+        res.append(root.val)
+        res += self.preorderTraversal(root.left)
+        res += self.preorderTraversal(root.right)
+        return res
