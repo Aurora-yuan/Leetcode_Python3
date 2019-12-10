8997629f8fe4a4c8d462b7f7b3f1a1ac1542a852
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-12-10 23:07:52 +0800

    Create 0402 移掉k位数字.py

---
+++ 0402 移掉k为数字/0402 移掉k位数字.py
@@ -0,0 +1,32 @@
+#label: 贪心+stack difficulty: medium
+
+"""
+思路：
+
+参考https://blog.csdn.net/u013075024/article/details/93197107
+
+"""
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        if len(num) <= k:
+            return '0'
+        if k == 0:
+            return num
+        stack,count=[],0
+        #对于字符串里的每一个元素
+        for i,x in enumerate(num):
+            #如果栈顶元素比待入栈元素要大，则删除栈顶元素，同时将待入栈元素push, count加1
+            while len(stack)>0 and x < stack[-1]:
+                stack.pop()
+                count+=1
+                #若入栈的元素已经为k
+                if count == k:
+                    #一般情况下，经过贪心算法的已经入栈的元素+num中未处理的元素
+                    #当有前导0时，由于先转为int,前导0消失，然后转为str类型，这就解决了前导0的问题
+                    return str(int(''.join(stack)+num[i:]))
+            stack.append(x)
+        #特殊情况下一直是升序，如123456，则只返回前len(num)-k个数
+        return str(int(''.join(stack[:len(num)-k])))
+
+
