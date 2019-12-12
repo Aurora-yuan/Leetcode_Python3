df4683c9618547dd13ae45b4e1476bde5503b8ef
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-12-12 22:33:35 +0800

    Create 0973 最接近原点点K个点.py

---
+++ 0973 最接近原点的K个点/0973 最接近原点点K个点.py
@@ -0,0 +1,6 @@
+#label: heap difficulty: medium
+
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        points.sort(key = lambda P: P[0]**2 + P[1]**2)
+        return points[:K]
