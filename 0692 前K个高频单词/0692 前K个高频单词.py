243f613abc135ea68b437acf7555d5dea821d434
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-12-13 22:25:49 +0800

    Create 0692 前K个高频单词.py

---
+++ 0692 前K个高频单词/0692 前K个高频单词.py
@@ -0,0 +1,9 @@
+#label: heap difficulty: medium
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        count = collections.Counter(words)
+        heap = [(-freq, word) for word, freq in count.items()]
+        heapq.heapify(heap)
+        return [heapq.heappop(heap)[1] for _ in range(k)]
+
