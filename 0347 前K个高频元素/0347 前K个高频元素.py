ae9f84d85f1fb7953744dbd0f6a12225349a8157
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-12-13 22:31:39 +0800

    Create 0347 前K个高频元素.py

---
+++ 0347 前K个高频元素/0347 前K个高频元素.py
@@ -0,0 +1,8 @@
+#label: heap difficulty: medium
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = collections.Counter(nums)   
+        return heapq.nlargest(k, count.keys(), key=count.get) 
+
+
