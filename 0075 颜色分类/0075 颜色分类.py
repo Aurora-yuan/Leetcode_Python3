cd876a14d2472f03ba157b16b5a338ade6cafbd3
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-12-17 22:30:22 +0800

    Create 0075 颜色分类.py

---
+++ 0075 颜色分类/0075 颜色分类.py
@@ -0,0 +1,20 @@
+#label: two pointers difficulty: medium
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        lo, hi = 0, len(nums) - 1
+        i = 0
+        while i <= hi:
+            x = nums[i]
+            if x == 0:
+                nums[lo], nums[i] = nums[i], nums[lo]
+                lo += 1
+                i += 1
+            elif x == 2:
+                nums[hi], nums[i] = nums[i], nums[hi]
+                hi -= 1
+            else:
+                i += 1
