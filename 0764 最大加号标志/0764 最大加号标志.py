710ca7c53b9a354eeee6ee70cfa565b88f2d239e
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-12-25 20:50:18 +0800

    Create 0764 最大加号标志.py

---
+++ 0764 最大加号标志/0764 最大加号标志.py
@@ -0,0 +1,41 @@
+#label: dynamic programming difficulty: medium
+
+"""
+思路：
+
+官方题解
+
+"""
+
+class Solution:
+    def orderOfLargestPlusSign(self, N: int, mines: List[List[int]]) -> int:
+        banned = {tuple(mine) for mine in mines}
+        dp = [[0] * N for _ in range(N)]
+        ans = 0
+        
+        for r in range(N):
+            count = 0
+            for c in range(N):
+                count = 0 if (r,c) in banned else count+1
+                dp[r][c] = count
+            
+            count = 0
+            for c in range(N-1, -1, -1):
+                count = 0 if (r,c) in banned else count+1
+                if count < dp[r][c]: dp[r][c] = count
+        
+        for c in range(N):
+            count = 0
+            for r in range(N):
+                count = 0 if (r,c) in banned else count+1
+                if count < dp[r][c]: dp[r][c] = count
+            
+            count = 0
+            for r in range(N-1, -1, -1):
+                count = 0 if (r, c) in banned else count+1
+                if count < dp[r][c]: dp[r][c] = count
+                if dp[r][c] > ans: ans = dp[r][c]
+        
+        return ans
+
+
