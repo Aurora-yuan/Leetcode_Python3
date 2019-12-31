751f91a58d7dfadee430637b33f50c1a0241af91
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2019-12-31 22:48:06 +0800

    Update 69 x的平方根.py

--- 69 x的平方根/69 x的平方根.py
+++ 69 x的平方根/69 x的平方根.py
@@ -21,3 +21,19 @@ class Solution:
           else:
             return mid
         return hi
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if not x:
+            return 0
+        if x < 4:
+            return 1
+        start,end = 2, x//2
+        while 1:
+            i = (start+end) // 2
+            if i ** 2 <= x and (i+1) **2 > x:
+                return i
+            if i ** 2 < x:
+                start = i+1
+            if i ** 2 > x:
+                end = i -1
