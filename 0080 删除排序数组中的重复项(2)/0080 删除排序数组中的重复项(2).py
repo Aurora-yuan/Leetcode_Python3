3f762ec0594b6e042988c65b7d9099157272d37b
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2020-02-05 19:39:27 +0800

    Create 0080 删除排序数组中的重复项(2).py

---
+++ 0080 删除排序数组中的重复项(2)/0080 删除排序数组中的重复项(2).py
@@ -0,0 +1,11 @@
+#label: 双指针 difficulty: medium
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        for num in nums:
+            if i < 2 or num != nums[i-2]:
+                nums[i] = num
+                i += 1
+        
+        return i
