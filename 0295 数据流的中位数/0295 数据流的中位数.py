15dfb58ba62ba34924951960ed2e06c8225e78b2
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2020-02-16 16:20:03 +0800

    Update 0295 数据流的中位数.py

--- 0295 数据流的中位数/0295 数据流的中位数.py
+++ 0295 数据流的中位数/0295 数据流的中位数.py
@@ -23,3 +23,34 @@
 
 """
 
+from heapq import *
+class MedianFinder:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.min_h = list()
+        self.max_h = list()
+        heapify(self.min_h)
+        heapify(self.max_h)
+        
+
+    def addNum(self, num: int) -> None:
+        heappush(self.min_h,num)
+        heappush(self.max_h,-heappop(self.min_h))
+        if len(self.max_h) > len(self.min_h):
+            heappush(self.min_h,-heappop(self.max_h))
+
+    def findMedian(self) -> float:
+        max_len = len(self.max_h)
+        min_len = len(self.min_h)
+        if max_len == min_len: #有两个候选中位数
+            return (self.min_h[0] + -self.max_h[0]) / 2.
+        else:#小顶堆的size 一定 >= 大顶堆的size，所以答案就是小顶堆的堆顶
+            return self.min_h[0] / 1.
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
