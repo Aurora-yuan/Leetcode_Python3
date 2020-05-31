e8c9f8522d46a5633ca711fbb7bd7165b76931f0
Aurora-yuan <55121811+Aurora-yuan@users.noreply.github.com>
2020-05-31 20:07:24 +0800

    Create 0115 不同的子序列.py

---
+++ 0115 不同的子序列/0115 不同的子序列.py
@@ -0,0 +1,19 @@
+#label: dynamic programming difficulty: difficult
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        #https://leetcode-cn.com/problems/distinct-subsequences/solution/dong-tai-gui-hua-by-powcai-5/
+        #dp[i][j]代表s[:j]的子序列中t[:i]出现的个数。
+        n1 = len(s)
+        n2 = len(t)
+        dp = [[0] * (n1 + 1) for _ in range(n2 + 1)]
+        for j in range(n1 + 1):
+            dp[0][j] = 1
+        for i in range(1, n2 + 1):
+            for j in range(1, n1 + 1):
+                if t[i - 1] == s[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]  + dp[i][j - 1]
+                else:
+                    dp[i][j] = dp[i][j - 1]
+        #print(dp)
+        return dp[-1][-1]
